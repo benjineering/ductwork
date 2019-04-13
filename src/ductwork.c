46a6af47c0562077f50731d121934b903315acae
Ben Williams <8enwilliams@gmail.com>
2019-04-13 18:02:43 +1000

    path setter

--- src/ductwork.c
+++ src/ductwork.c
@@ -79,7 +79,6 @@ dw_instance *dw_init(
   void *userData
 ) {
   dw_instance *dw = (dw_instance *)malloc(sizeof(dw_instance));
-  dw->path = requestedPath;
   dw->errorHandler = errorHandler;
   dw->type = type;
   dw->userData = userData;
@@ -88,12 +87,12 @@ dw_instance *dw_init(
   pthread_cond_init(&dw->openThread->condition, NULL);
   pthread_mutex_init(&dw->openThread->mutex, NULL);
 
-  if ((strlen(dw->path) + 1) > DW_FULL_PATH_SIZE) {
+  if (requestedPath && (strlen(requestedPath) + 1) > DW_FULL_PATH_SIZE) {
     throw_last_error(dw, "Full path buffer overrun");
     return NULL;
   }
 
-  strcpy((char *)dw->fullPath, dw->path);
+  dw_set_path(dw, requestedPath);
   return dw;
 }
 
@@ -158,6 +157,13 @@ void dw_open_pipe(
 
 const char *dw_get_full_path(dw_instance *dw) {
   return dw->fullPath;
+}
+
+void dw_set_path(dw_instance *dw, const char *path) {
+  if (path) {
+    dw->path = path;
+    strcpy((char *)dw->fullPath, dw->path);
+  }
 }
 
 void *dw_get_user_data(dw_instance *dw) {
