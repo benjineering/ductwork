daf214869aa347f1ce6a48605336b7f0b7ca4ce2
Ben Williams <8enwilliams@gmail.com>
2019-07-10 20:21:44 +1000

    added all c++ methods

--- src/c++/ductwork.cpp
+++ src/c++/ductwork.cpp
@@ -12,6 +12,38 @@ Server::~Server() {
     dw_free(dw);
 }
 
+bool Server::createPipe(int defaultTimeoutMs) {
+  return dw_create_pipe(dw, defaultTimeoutMs);
+}
+
+void Server::close_pipe() {
+  dw_close_pipe(dw);
+}
+
 string Server::get_full_path() {
   return dw_get_full_path(dw);
+}
+
+void Server::set_path(string path) {
+  dw_set_path(dw, path.c_str());
+}
+
+int Server::get_fd() {
+  return dw_get_fd(dw);
+}
+
+void *Server::get_user_data() {
+  return dw_get_user_data(dw);
+}
+
+void Server::set_user_data(void *userData) {
+  dw_set_user_data(dw, userData);
+}
+
+enum dw_instance_type Server::get_type() {
+  return dw_get_type(dw);
+}
+
+string Server::get_last_error() {
+  return string(dw_get_last_error(dw));
 }
--- src/c++/ductwork.h
+++ src/c++/ductwork.h
@@ -7,7 +7,18 @@ namespace Ductwork {
     Server(std::string path, void *userData);
     ~Server();
 
+    bool createPipe(int defaultTimeoutMs);
+    void close_pipe();
+    
     std::string get_full_path();
+    void set_path(std::string path);
+    int get_fd();
+
+    void *get_user_data();
+    void set_user_data(void *userData);
+
+    enum dw_instance_type get_type();
+    std::string get_last_error();
 
   private:
     dw_instance *dw;
